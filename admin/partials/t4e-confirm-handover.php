cbd9ea4eef15f0db300889abfa0a0bcea06ba178
Tarikul Islam <34490496+tarikul47@users.noreply.github.com>
2025-10-15 04:39:51 +0600

    Plugin optimized

--- admin/partials/t4e-confirm-handover.php
+++ admin/partials/t4e-confirm-handover.php
@@ -8,7 +8,7 @@
             ?>
     </p>
     <button id="t4e-confirm-handover-button" class="button-primary" type="button" onclick="t4eConfirmHandover()">
-        <?php echo esc_html__("Confirm Handover (Custom)", "trustap-payment-gateway") ?>
+        <?php echo esc_html__("Confirm Handover", "trustap-payment-gateway") ?>
     </button>
     <div id="t4e-handover-spinner" style="display: none;">
         <div class="t4e-spinner"></div>
